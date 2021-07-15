70b8e21519902fd50eae4316c4df4638aa0d66d3
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2021-07-14 17:18:48 -0700

    Create entity class
    Also used transformation matrix in vertex shader

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -9,7 +9,7 @@ uniform mat4 transformationMatrix;
 
 void main(void) {
 
-	gl_Position = vec4(position.x, position.y, position.z, 1.0);
+	gl_Position = transformationMatrix * vec4(position.x, position.y, position.z, 1.0);
 	passTextureCoords = textureCoords;
 
 }
